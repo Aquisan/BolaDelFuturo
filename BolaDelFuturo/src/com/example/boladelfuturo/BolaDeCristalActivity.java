49b03ceda95e947fe42217e2d70dc14610f692c8
Carlos Tessier <prof.carlos.tessier@gmail.com>
2014-11-10 11:31:19 +0100

    added animation

--- BolaDelFuturo/src/com/example/boladelfuturo/BolaDeCristalActivity.java
+++ BolaDelFuturo/src/com/example/boladelfuturo/BolaDeCristalActivity.java
@@ -1,9 +1,11 @@
 package com.example.boladelfuturo;
 
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class BolaDeCristalActivity extends ActionBarActivity {
@@ -19,16 +21,27 @@ public class BolaDeCristalActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bola_de_cristal);
 		
-		
-		
 		answerLabel = (TextView) findViewById(R.id.textView1);
+				
 		//background = (RelativeLayout) findViewById(R.id.background);		
 		
 	}
 
 	public void questionAction(View v) {
 		
+		animateBall();
 		answerLabel.setText(MagicBall.getPrediction(this));
 		//background.setBackgroundColor(MagicBall.getColor());
+	}
+	
+	private void animateBall(){
+		
+		ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+		imageView.setImageResource(R.drawable.ball_animation);
+		AnimationDrawable animation = (AnimationDrawable) imageView.getDrawable();
+		if(animation.isRunning())
+			animation.stop();
+		
+		animation.start();
 	}
 }
