9143e1c47fc9b2cff4619954e91b342d27165c34
Carlos Tessier <prof.carlos.tessier@gmail.com>
2014-11-10 15:04:22 +0100

    added acelerometer sensor

--- BolaDelFuturo/src/com/example/boladelfuturo/BolaDeCristalActivity.java
+++ BolaDelFuturo/src/com/example/boladelfuturo/BolaDeCristalActivity.java
@@ -3,6 +3,8 @@ package com.example.boladelfuturo;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
@@ -11,38 +13,60 @@ import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.boladelfuturo.ShakeDetector.OnShakeListener;
+
 public class BolaDeCristalActivity extends Activity {
 	
 	// Declare our views variables
 	private TextView answerLabel;
+	private SensorManager mSensorManager;
+	private Sensor mAcelerometer;
+	private ShakeDetector mShakeDetector;
 	//private RelativeLayout background;
 	final static float TRANSPARENTE = 0;
 	final static float OPACO = 1;
 	final static long DURATION = 1500;
+	
 	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bola_de_cristal);
-		
+		//background = (RelativeLayout) findViewById(R.id.background);		
+
 		answerLabel = (TextView) findViewById(R.id.textView1);
 		
-				
-		//background = (RelativeLayout) findViewById(R.id.background);		
+		mSensorManager =  (SensorManager) getSystemService(SENSOR_SERVICE);
+		mAcelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		
+		mShakeDetector = new ShakeDetector(new OnShakeListener() {
+			
+			@Override
+			public void onShake() {
+				showAnswer();				
+			}
+		});	
+		
 		
 	}
 
+	
+	
 	public void questionAction(View v) {
 		
+		showAnswer();		
 		
+	}
+	
+	public void showAnswer(){
 		answerLabel.setText(MagicBall.getPrediction(this));
 		//background.setBackgroundColor(MagicBall.getColor());
 		animateBall();
 		animateAnswer();
 		playSound();
-		
 	}
+	
 	
 	private void animateBall(){
 		
@@ -75,4 +99,18 @@ public class BolaDeCristalActivity extends Activity {
 			}
 		});
 	}	
+	
+	@Override
+	 protected void onResume() {
+         super.onResume();
+ 		mSensorManager.registerListener(mShakeDetector, mAcelerometer, mSensorManager.SENSOR_DELAY_UI);
+     }
+
+	@Override
+     protected void onPause() {
+         super.onPause();
+         mSensorManager.unregisterListener(mShakeDetector);
+     }
+     
+     
 }
